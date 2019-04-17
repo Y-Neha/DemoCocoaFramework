43bb2f8647eac4a2ac5d98b3c71e3be8e7af895b
Neha Yadav <neha.yadav@citiustech.com>
2019-04-17 19:25:31 +0530

    jbjk

---
+++ DemoCocoaFramework/MyFramework.swift
@@ -0,0 +1,14 @@
+//
+//  MyFramework.swift
+//  DemoCocoaFramework
+//
+//  Created by Neha Yadav on 17/04/19.
+//  Copyright © 2019 Neha Yadav. All rights reserved.
+//
+
+import Foundation
+
+public class MyFramework {
+    static let shared = MyFramework()
+    func foo() {}
+}   
